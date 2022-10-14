ace9624a0f6ae188d1c25f2ff1fb71ecb0ccbd8d
sharumario <sharumario@mail.ru>
2022-10-14 12:52:04 +0500

    Deploy

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -94,9 +94,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/reviews/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'reviews/static')
+STATICFILES_DIRS = [BASE_DIR + '/reviews/static/']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
